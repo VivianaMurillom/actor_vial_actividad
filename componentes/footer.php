6b73daf8d918bad578ebf04d9c1895e25bbb804c
Viviana Murillo <85419875+VivianaMurillom@users.noreply.github.com>
2024-06-28 19:26:35 -0500

    Se agrega archivos de la carpeta componentes

---
+++ componentes/footer.php
@@ -0,0 +1,15 @@
+<footer>
+    <div class='leftFooter'>
+        <img src='recursos/logo-cafam.png' class='logoCafam' alt='Logo de Cafam CET'></img>
+    </div>
+    <div class='centerFooter'>
+        <p class='thanks'>Con <span>cariño</span> para el mundo</p>
+        <p class='version'>Versión N° <span>1</span> de <span>2024</span></p>
+    </div>
+    <div class='centerFooter'>
+        <div class='links'>
+        <p class='thanks'>Actividad educativa para el programa técnico laboral en </p>
+        <p class='thanks'><p class='thanks'><span>Procesamiento de datos</span></p>
+        </div>
+    </div>
+</footer>
---
+++ componentes/navegador.php
@@ -0,0 +1,22 @@
+<nav>
+    <ul class="nav justify-content-center">
+    <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="principal.php">Inicio</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="perfil.php">Actualiza tu perfil</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="contenido.php">Estudia</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="evaluacion.php">Evalúate</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="agradecimientos.php">Agradecimientos</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="no_registro.php">Cerrar sesión</a>
+    </li>
+    </ul>
+</nav>
